483189a07f8c6e881c5de1401f68887b5a751040
안철민 <81482706+cjfals9626@users.noreply.github.com>
2023-12-23 23:51:08 +0900

    종이 조각

---
+++ 14300/BOJ_14391.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        int max = 0;
+
+        for (int s = 0; s < (1 << n * m); s++) {
+            int sum = 0;
+
+
+            for (int i = 0; i < n; i++) {
+                int cur = 0;
+                for (int j = 0; j < m; j++) {
+                    int k = i * m + j;
+
+                    if ((s & (1 << k)) == 0) {
+                        cur *= 10;
+                        cur += arr[i][j];
+                    } else {
+                        sum += cur;
+                        cur = 0;
+                    }
+                }
+                sum += cur;
+            }
+
+            for (int j = 0; j < m; j++) {
+                int cur = 0;
+                for (int i = 0; i < n; i++) {
+                    int k = i * m + j;
+
+                    if ((s & (1 << k)) != 0) {
+                        cur *= 10;
+                        cur += arr[i][j];
+                    } else {
+                        sum += cur;
+                        cur = 0;
+                    }
+                }
+                sum += cur;
+            }
+
+            max = Math.max(max, sum);
+        }
+
+
+        System.out.println(max);
+    }
+}
