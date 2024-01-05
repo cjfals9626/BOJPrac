b9686642b4fd1fa997f0e04e780df9f30de0f90d
안철민 <81482706+cjfals9626@users.noreply.github.com>
2024-01-05 20:27:09 +0900

    Base Conversion

--- Prac/src/Test.java
+++ Prac/src/Test.java
@@ -1,11 +1,7 @@
 public class Test {
 
-    static boolean[][] visited = new boolean[1001][1001];//[clipboard][screen]
-
     public static void main(String[] args) {
-        Long nums = 1L;
-        Long base = 2L;
-        System.out.println(nums/base);
+
 
     }
 }
